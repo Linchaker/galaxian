5928756535500b73921b1e4d3f89b964230d5312
Linchaker <linchaker@gmail.com>
2023-01-22 02:06:52 +0200

    initial commit

---
+++ config.py
@@ -0,0 +1,17 @@
+class Config:
+
+    def __init__(self):
+        # Screen settings
+        self.screen_width = 1380
+        self.screen_height = 768
+        self.bg_color = "#A6A6AA"
+
+        # Ship settings
+        self.ship_speed = 1.5
+
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_with = 3
+        self.bullet_height = 20
+        self.bullet_color = "darkred"
+        self.bullet_limit = 10
