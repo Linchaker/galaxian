fbb175581a3154727b4fd576462b26e4958958fc
Linchaker <linchaker@gmail.com>
2023-01-25 16:28:37 +0200

    feat: increase the difficulty of the level

--- config.py
+++ config.py
@@ -7,19 +7,29 @@ class Config:
         self.bg_color = "#A6A6AA"
 
         # Ship settings
-        self.ship_speed = 3.5
         self.ship_limit = 3
 
         # Bullet settings
-        self.bullet_speed = 2.0
         self.bullet_with = 10
         self.bullet_height = 20
         self.bullet_color = "darkred"
         self.bullet_limit = 10
 
         # Aliens
-        self.alien_speed = 2.0
         self.fleet_drop_speed = 100
+
+        self.speedup_scale = 1.1
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        self.ship_speed = 1.5
+        self.bullet_speed = 3.0
+        self.alien_speed = 1.0
+
         # right 1, left -1
         self.fleet_direction = 1
 
+    def increase_speed(self):
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
--- index.py
+++ index.py
@@ -85,6 +85,7 @@ class Galaxian:
         """Start game by Play button"""
         button_clicked = self.button_play.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+            self.config.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.stats.game_active = True
 
@@ -167,6 +168,7 @@ class Galaxian:
             # new level
             self.bullets.empty()
             self._create_fleet()
+            self.config.increase_speed()
 
     def _check_aliens_bottom(self):
         """Check if alien is on bottom of screen"""
